d250b54504c39e4fa5a219edb14fe1da81c8652f
RafaelMP <rmorales@linko.mx>
2018-12-31 17:04:47 -0600

    Fin sección 5

--- routes/router.ts
+++ routes/router.ts
@@ -1,6 +1,7 @@
 
 
 import { Router, Request, Response } from 'express';
+import Server from '../classes/server';
 
 
 const router = Router();
@@ -19,6 +20,16 @@ router.post('/mensajes', ( req: Request, res: Response) => {
     const cuerpo = req.body.cuerpo;
     const de = req.body.de;
 
+    const payload = {
+        de,
+        cuerpo
+    };
+
+
+    const server = Server.instance;
+
+    server.io.emit( 'mensaje-nuevo', payload );
+
     res.json({
         ok: true,
         cuerpo,
@@ -32,6 +43,15 @@ router.post('/mensajes/:id', ( req: Request, res: Response) => {
     const cuerpo = req.body.cuerpo;
     const de = req.body.de;
     const id = req.params.id;
+
+    const payload = {
+        de,
+        cuerpo
+    };
+
+    const server = Server.instance;
+
+    server.io.in( id ).emit( 'mensaje-privado', payload );
 
     res.json({
         ok: true,
--- sockets/socket.ts
+++ sockets/socket.ts
@@ -1,27 +1,49 @@
 import { Socket } from "socket.io";
+import socketIO from 'socket.io';
+import { UsuarioLista } from "../classes/usuarios-lista";
+import { Usuario } from "../classes/usuario";
 
 
+export const usuariosConectados = new UsuarioLista();
 
 
+export const conectarCliente = ( cliente: Socket ) => {
 
+    const usuario = new Usuario( cliente.id );
+    usuariosConectados.agregar( usuario );
+
+}
 
 
 export const desconectar = ( cliente: Socket ) => {
 
     cliente.on( 'disconnect', () => {
         console.log( 'Cliente desconectado' );
+        usuariosConectados.borrarUsuario( cliente.id );
     });
 
 }
 
 
 // Escuchar mensajes
-export const mensaje = ( cliente: Socket, io: SocketIO.Server ) => {
+export const mensaje = ( cliente: Socket, io: socketIO.Server ) => {
     cliente.on( 'mensaje', ( payload: { de: string, cuerpo: string } ) => {
 
         console.log( 'Mensaje recibido', payload );
 
         io.emit( 'mensaje-nuevo', payload );
         
+    });
+}
+
+// Configurar usuario
+export const configurarUsuario = ( cliente: Socket, io: socketIO.Server ) => {
+    cliente.on( 'configurar-usuario', ( payload: { nombre: string }, callback: Function ) => {
+        usuariosConectados.actualizaNombre( cliente.id, payload.nombre );
+
+        callback({
+            ok: true,
+            mensaje: `Usuario ${ payload.nombre }, configurado`
+        });
     });
 }
